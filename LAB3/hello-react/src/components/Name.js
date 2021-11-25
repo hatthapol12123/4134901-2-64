970167d5362e2ebabc621d63610f4074735541d5
hatthapol12123 <hatthapol12123@gmail.com>
2021-11-25 15:34:43 +0700

    commit

---
+++ LAB3/hello-react/src/components/Name.js
@@ -0,0 +1,10 @@
+function MyName(){
+    return(
+        <div className="MyName">
+            <div>นายหัตพล นามมนตรี</div>
+            <div>610112418033</div>
+        </div>
+    )
+}
+
+export default MyName;
